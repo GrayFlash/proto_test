a26c5de34814b0fa115f8c2304b1a56943dbaccd
Gaurav Kumar <gk74533@gmail.com>
2022-07-15 11:31:50 +0530

    feat: use oneof and any

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module example.com/m/v2
 
 go 1.18
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/m/v2/github.com/GrayFlash/proto_test"
+	"google.golang.org/protobuf/types/known/anypb"
+	// "github.com/GrayFlash/proto_test"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	db := &proto_test.Dashboard{}
+	any, err := anypb.New(db)
+	if err != nil {
+		fmt.Println(err)
+	}
+	data := proto_test.Asset{Data: any}
+	fmt.Println(data.Data)
+
+	one := proto_test.AssetOne{Data: &proto_test.AssetOne_Dashboard{Dashboard: &proto_test.Dashboard{}}}
+	fmt.Println(one.Data)
 }
